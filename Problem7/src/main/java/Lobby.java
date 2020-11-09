8a2f558570a7feb50054c77483cb08d8f2bc8cbe
Stas Dombrovskyi <727727stas@gmail.com>
2020-11-09 14:54:44 +0200

    Update Lobby.java

--- Problem7/src/main/java/Lobby.java
+++ Problem7/src/main/java/Lobby.java
@@ -5,30 +5,31 @@ public class Lobby {
     private static Runnable startingGame = new Runnable() {
         @Override
         public void run() {
-            synchronized (this) {
-                try {
-                    this.wait(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-                System.out.println("Game started!");
+            System.out.println("Game started!");
         }
     };
 
     private static final CyclicBarrierCustom barrier = new CyclicBarrierCustom(5, startingGame);
 
-    public static void addPlayer(int playerId){
+    public static void addPlayer(int playerId) {
         new Thread() {
             @Override
-            public void run(){
+            public void run() {
                 try {
+                    Random random = new Random();
                     synchronized (this) {
-                        Random random = new Random();
                         this.wait(random.nextInt(4000) + 1000);
                     }
                     System.out.println("Player " + playerId + " entered the lobby");
                     barrier.await();
+
+                    synchronized (this) {
+                        this.wait(random.nextInt(4000) + 3000);
+                    }
+
+                    System.out.println("Player " + playerId + " entered the lobby");
+                    barrier.await();
+
                 } catch (InterruptedException | BrokenBarrierException e) {
                     e.printStackTrace();
                 }
