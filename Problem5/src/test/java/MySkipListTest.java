b91bf9b630fc60710aaef1e2e319416aa19e925f
Stas Dombrovskyi <727727stas@gmail.com>
2020-11-09 14:54:38 +0200

    Update MySkipListTest.java

--- Problem5/src/test/java/MySkipListTest.java
+++ Problem5/src/test/java/MySkipListTest.java
@@ -11,19 +11,6 @@ public class MySkipListTest {
     @Before
     public void setUp(){
         skipList = new MySkipList(16);
-
-        skipList.add(5);
-        skipList.add(1);
-        skipList.add(3);
-        skipList.add(4);
-        skipList.add(6);
-        skipList.add(13);
-        skipList.add(511);
-        skipList.add(111);
-        skipList.add(332);
-        skipList.add(522);
-        skipList.add(112);
-        skipList.add(3323);
     }
 
     private boolean checkListOrder(){
@@ -43,23 +30,28 @@ public class MySkipListTest {
     }
 
     @Test
-    public void contains_TEST(){
-        Assert.assertTrue(skipList.contains(1));
-        Assert.assertTrue(skipList.contains(3));
-        Assert.assertTrue(skipList.contains(4));
-        Assert.assertTrue(skipList.contains(5));
-        Assert.assertTrue(skipList.contains(6));
-        Assert.assertTrue(skipList.contains(13));
-        Assert.assertTrue(skipList.contains(111));
-        Assert.assertTrue(skipList.contains(112));
-        Assert.assertTrue(skipList.contains(332));
-        Assert.assertTrue(skipList.contains(511));
-        Assert.assertTrue(skipList.contains(522));
-        Assert.assertTrue(skipList.contains(3323));
-        Assert.assertFalse(skipList.contains(1111));
-        Assert.assertFalse(skipList.contains(0));
-        Assert.assertFalse(skipList.contains(32321));
-        Assert.assertFalse(skipList.contains(145312));
+    public void concurrentDuplicateAdd_TEST() throws InterruptedException {
+        Runnable thread2;
+        thread2 = () -> {
+            for(int i = 0; i < 100; i++){
+                skipList.add(200 + i);
+            }
+        };
+
+        Thread testThread = new Thread(thread2);
+        testThread.start();
+
+        for(int i = 0; i < 100; i++){
+            skipList.add(200 + i);
+        }
+
+        testThread.join();
+
+        for(int i = 0; i < 100; i++){
+            Assert.assertTrue(skipList.contains(200 + i));
+        }
+
+        System.out.println(skipList.getSize());
 
         Assert.assertTrue(checkListOrder());
     }
@@ -138,31 +130,6 @@ public class MySkipListTest {
 
         for(int i = 0; i < 50; i++){
             skipList.remove(200 + i * 2 + 1);
-        }
-
-        testThread.join();
-
-        for(int i = 0; i < 100; i++){
-            Assert.assertFalse(skipList.contains(200 + i));
-        }
-
-        Assert.assertTrue(checkListOrder());
-    }
-
-    @Test
-    public void concurrentMixedOperation_TEST() throws InterruptedException {
-        Runnable thread2;
-        thread2 = () -> {
-            for(int i = 0; i < 50; i++){
-                skipList.remove(200 + i);
-            }
-        };
-
-        Thread testThread = new Thread(thread2);
-        testThread.start();
-
-        for(int i = 0; i < 50; i++){
-            skipList.add(200 + i);
         }
 
         testThread.join();
